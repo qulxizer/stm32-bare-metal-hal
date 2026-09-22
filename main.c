e5b40b3f646c1158644f7fb0609c25999afa9cdd
mahdi <mahdi@1jz.cc>
2026-09-23 01:05:24 +0300

    blinky

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "stm32g0xx_hal.h"
+
+int main(void) {
+  HAL_Init();
+
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  GPIO_InitTypeDef gpio = {
+      .Pin = GPIO_PIN_8,
+      .Mode = GPIO_MODE_OUTPUT_PP,
+      .Pull = GPIO_NOPULL,
+      .Speed = GPIO_SPEED_FREQ_LOW,
+  };
+
+  HAL_GPIO_Init(GPIOA, &gpio);
+
+  while (1) {
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_8);
+    HAL_Delay(500);
+  }
+}
